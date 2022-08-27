cb7e2abdfd995e5847c990cfa3184e0c1d9b6d39
Ryan Short <110632551+RyanShort13110@users.noreply.github.com>
2022-08-27 09:35:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,12 @@
 function arithmetic(a, b, operator){
   switch(operator) {
     case 'add':
-      return a + b
+      return a + b;
     case 'subtract':
-      return a - b
+      return a - b;
     case 'multiply':
-      return a * b
+      return a * b;
     case 'divide':
-      return a / b
+      return a / b;
   }
-}
+};
